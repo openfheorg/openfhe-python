f67bfafec2efe6aebe709bfb60e65c60cf8960f3
Rener Oliveira (Ubuntu WSL) <rener.oliveira@fgv.edu.br>
2023-03-12 23:26:21 -0300

    initial python serial example

---
+++ src/pke/examples/simple-integers-serial.py
@@ -0,0 +1,24 @@
+# Initial Settings
+from openfhe import *
+# import openfhe.PKESchemeFeature as Feature
+
+datafolder = 'demoData'
+
+print("This program requres the subdirectory `" + datafolder + "' to exist, otherwise you will get an error writing serializations.")
+
+# Sample Program: Step 1: Set CryptoContext
+parameters = CCParamsBFVRNS()
+parameters.SetPlaintextModulus(65537)
+parameters.SetMultiplicativeDepth(2)
+
+cryptoContext = GenCryptoContext(parameters)
+# Enable features that you wish to use
+cryptoContext.Enable(PKESchemeFeature.PKE)
+cryptoContext.Enable(PKESchemeFeature.KEYSWITCH)
+cryptoContext.Enable(PKESchemeFeature.LEVELEDSHE)
+
+## Not working yet
+if not SerializeToFile(datafolder + "/cryptocontext.txt", cryptoContext, 'binary'):
+   raise Exception("Error writing serialization of the crypto context to cryptocontext.txt")
+
+
