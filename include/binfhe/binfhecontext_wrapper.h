e1a880f708f9d5f3c3d3fe47fa5119d124a454d0
Rener Oliveira (Ubuntu WSL) <rener.oliveira@fgv.edu.br>
2023-07-27 07:06:41 -0300

    license copy

--- include/binfhe/binfhecontext_wrapper.h
+++ include/binfhe/binfhecontext_wrapper.h
@@ -1,22 +1,19 @@
-//==================================================================================
 // BSD 2-Clause License
-//
-// Copyright (c) 2014-2023, NJIT, Duality Technologies Inc. and other contributors
-//
+
+// Copyright (c) 2023, OpenFHE
+
 // All rights reserved.
-//
-// Author TPOC: contact@openfhe.org
-//
+
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-//
+
 // 1. Redistributions of source code must retain the above copyright notice, this
 //    list of conditions and the following disclaimer.
-//
+
 // 2. Redistributions in binary form must reproduce the above copyright notice,
 //    this list of conditions and the following disclaimer in the documentation
 //    and/or other materials provided with the distribution.
-//
+
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -27,7 +24,6 @@
 // CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//==================================================================================
 
 #ifndef BINFHE_CRYPTOCONTEXT_BINDINGS_H
 #define BINFHE_CRYPTOCONTEXT_BINDINGS_H
