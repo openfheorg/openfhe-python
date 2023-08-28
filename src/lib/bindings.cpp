ebb575a52ccc312a94b40795e0cd0a15b759bfd9
Gergo, Petrik <gergo.petrik@xtendr.io>
2023-08-28 11:56:05 +0200

    serialization works for ckks, added missing serialization methods, privatekey has constructor now

--- src/lib/bindings.cpp
+++ src/lib/bindings.cpp
@@ -845,13 +845,8 @@ void bind_enums_and_constants(py::module &m)
 void bind_keys(py::module &m)
 {
     py::class_<PublicKeyImpl<DCRTPoly>, std::shared_ptr<PublicKeyImpl<DCRTPoly>>>(m, "PublicKey")
-        .def(py::init<>())
-        .def("GetKeyTag", &PublicKeyImpl<DCRTPoly>::GetKeyTag)
-        .def("SetKeyTag", &PublicKeyImpl<DCRTPoly>::SetKeyTag);
-    py::class_<PrivateKeyImpl<DCRTPoly>, std::shared_ptr<PrivateKeyImpl<DCRTPoly>>>(m, "PrivateKey")
-        .def(py::init<>())
-        .def("GetKeyTag", &PrivateKeyImpl<DCRTPoly>::GetKeyTag)
-        .def("SetKeyTag", &PrivateKeyImpl<DCRTPoly>::SetKeyTag);
+        .def(py::init<>());
+    py::class_<PrivateKeyImpl<DCRTPoly>, std::shared_ptr<PrivateKeyImpl<DCRTPoly>>>(m, "PrivateKey");
     py::class_<KeyPair<DCRTPoly>>(m, "KeyPair")
         .def_readwrite("publicKey", &KeyPair<DCRTPoly>::publicKey)
         .def_readwrite("secretKey", &KeyPair<DCRTPoly>::secretKey)
