820ba008f82343fa836122a1d4898e2fdf6a0378
Drew Dolgert <adolgert@andrew.cmu.edu>
2024-02-29 15:07:31 -0500

    adding basic tests to dev work

---
+++ tests/test_cryptocontext.py
@@ -0,0 +1,17 @@
+import pytest
+import openfhe as fhe
+
+
+@pytest.mark.long
+@pytest.mark.skipif(fhe.get_native_int() < 80, reason="Only for NATIVE_INT=128")
+@pytest.mark.parametrize("scaling", [fhe.FIXEDAUTO, fhe.FIXEDMANUAL])
+def test_ckks_context(scaling):
+    batch_size = 8
+    parameters = fhe.CCParamsCKKSRNS()
+    parameters.SetMultiplicativeDepth(5)
+    parameters.SetScalingModSize(78)
+    parameters.SetBatchSize(batch_size)
+    parameters.SetScalingTechnique(scaling)
+    parameters.SetNumLargeDigits(2)
+    cc = fhe.GenCryptoContext(parameters)
+    assert isinstance(cc, fhe.CryptoContext)
