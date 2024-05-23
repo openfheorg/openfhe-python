ad69449d5b07debea8f1dca98cb2ee4113e709d9
Muthu Annamalai <photonique@hey.com>
2024-05-23 05:19:29 +0000

    #53: add initial unittests        - iterate through boolean AND OR NOT test        - test all the examples from the unittest for run through
           - run black code formatter
           - update CMakeLists.txt for the situation of python3.12 build
           - add tests

--- examples/pke/simple-integers.py
+++ examples/pke/simple-integers.py
@@ -1,94 +1,103 @@
 # Initial Settings
 from openfhe import *
+
 # import openfhe.PKESchemeFeature as Feature
 
 
-# Sample Program: Step 1: Set CryptoContext
-parameters = CCParamsBFVRNS()
-parameters.SetPlaintextModulus(65537)
-parameters.SetMultiplicativeDepth(2)
+def main():
+    # Sample Program: Step 1: Set CryptoContext
+    parameters = CCParamsBFVRNS()
+    parameters.SetPlaintextModulus(65537)
+    parameters.SetMultiplicativeDepth(2)
 
-crypto_context = GenCryptoContext(parameters)
-# Enable features that you wish to use
-crypto_context.Enable(PKESchemeFeature.PKE)
-crypto_context.Enable(PKESchemeFeature.KEYSWITCH)
-crypto_context.Enable(PKESchemeFeature.LEVELEDSHE)
+    crypto_context = GenCryptoContext(parameters)
+    # Enable features that you wish to use
+    crypto_context.Enable(PKESchemeFeature.PKE)
+    crypto_context.Enable(PKESchemeFeature.KEYSWITCH)
+    crypto_context.Enable(PKESchemeFeature.LEVELEDSHE)
 
-# Sample Program: Step 2: Key Generation
+    # Sample Program: Step 2: Key Generation
 
-# Generate a public/private key pair
-key_pair = crypto_context.KeyGen()
+    # Generate a public/private key pair
+    key_pair = crypto_context.KeyGen()
 
-# Generate the relinearization key
-crypto_context.EvalMultKeyGen(key_pair.secretKey)
+    # Generate the relinearization key
+    crypto_context.EvalMultKeyGen(key_pair.secretKey)
 
-# Generate the rotation evaluation keys
-crypto_context.EvalRotateKeyGen(key_pair.secretKey, [1, 2, -1, -2])
+    # Generate the rotation evaluation keys
+    crypto_context.EvalRotateKeyGen(key_pair.secretKey, [1, 2, -1, -2])
 
-# Sample Program: Step 3: Encryption
+    # Sample Program: Step 3: Encryption
 
-# First plaintext vector is encoded
-vector_of_ints1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-plaintext1 = crypto_context.MakePackedPlaintext(vector_of_ints1)
+    # First plaintext vector is encoded
+    vector_of_ints1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    plaintext1 = crypto_context.MakePackedPlaintext(vector_of_ints1)
 
-# Second plaintext vector is encoded
-vector_of_ints2 = [3, 2, 1, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-plaintext2 = crypto_context.MakePackedPlaintext(vector_of_ints2)
+    # Second plaintext vector is encoded
+    vector_of_ints2 = [3, 2, 1, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    plaintext2 = crypto_context.MakePackedPlaintext(vector_of_ints2)
 
-# Third plaintext vector is encoded
-vector_of_ints3 = [1, 2, 5, 2, 5, 6, 7, 8, 9, 10, 11, 12]
-plaintext3 = crypto_context.MakePackedPlaintext(vector_of_ints3)
+    # Third plaintext vector is encoded
+    vector_of_ints3 = [1, 2, 5, 2, 5, 6, 7, 8, 9, 10, 11, 12]
+    plaintext3 = crypto_context.MakePackedPlaintext(vector_of_ints3)
 
-# The encoded vectors are encrypted
-ciphertext1 = crypto_context.Encrypt(key_pair.publicKey, plaintext1)
-ciphertext2 = crypto_context.Encrypt(key_pair.publicKey, plaintext2)
-ciphertext3 = crypto_context.Encrypt(key_pair.publicKey, plaintext3)
+    # The encoded vectors are encrypted
+    ciphertext1 = crypto_context.Encrypt(key_pair.publicKey, plaintext1)
+    ciphertext2 = crypto_context.Encrypt(key_pair.publicKey, plaintext2)
+    ciphertext3 = crypto_context.Encrypt(key_pair.publicKey, plaintext3)
 
-#  Sample Program: Step 4: Evaluation
+    #  Sample Program: Step 4: Evaluation
 
-# Homomorphic additions
-ciphertext_add12 = crypto_context.EvalAdd(ciphertext1, ciphertext2)
-ciphertext_add_result = crypto_context.EvalAdd(ciphertext_add12, ciphertext3)
+    # Homomorphic additions
+    ciphertext_add12 = crypto_context.EvalAdd(ciphertext1, ciphertext2)
+    ciphertext_add_result = crypto_context.EvalAdd(ciphertext_add12, ciphertext3)
 
-# Homomorphic Multiplication
-ciphertext_mult12 = crypto_context.EvalMult(ciphertext1, ciphertext2)
-ciphertext_mult_result = crypto_context.EvalMult(ciphertext_mult12, ciphertext3)
+    # Homomorphic Multiplication
+    ciphertext_mult12 = crypto_context.EvalMult(ciphertext1, ciphertext2)
+    ciphertext_mult_result = crypto_context.EvalMult(ciphertext_mult12, ciphertext3)
 
-# Homomorphic Rotations
-ciphertext_rot1 = crypto_context.EvalRotate(ciphertext1, 1)
-ciphertext_rot2 = crypto_context.EvalRotate(ciphertext1, 2)
-ciphertext_rot3 = crypto_context.EvalRotate(ciphertext1, -1)
-ciphertext_rot4 = crypto_context.EvalRotate(ciphertext1, -2)
+    # Homomorphic Rotations
+    ciphertext_rot1 = crypto_context.EvalRotate(ciphertext1, 1)
+    ciphertext_rot2 = crypto_context.EvalRotate(ciphertext1, 2)
+    ciphertext_rot3 = crypto_context.EvalRotate(ciphertext1, -1)
+    ciphertext_rot4 = crypto_context.EvalRotate(ciphertext1, -2)
 
-# Sample Program: Step 5: Decryption
+    # Sample Program: Step 5: Decryption
 
-# Decrypt the result of additions
-plaintext_add_result = crypto_context.Decrypt(ciphertext_add_result,key_pair.secretKey)
+    # Decrypt the result of additions
+    plaintext_add_result = crypto_context.Decrypt(
+        ciphertext_add_result, key_pair.secretKey
+    )
 
-# Decrypt the result of multiplications
-plaintext_mult_result = crypto_context.Decrypt(ciphertext_mult_result,key_pair.secretKey)
+    # Decrypt the result of multiplications
+    plaintext_mult_result = crypto_context.Decrypt(
+        ciphertext_mult_result, key_pair.secretKey
+    )
 
-# Decrypt the result of rotations
-plaintextRot1 = crypto_context.Decrypt(ciphertext_rot1,key_pair.secretKey)
-plaintextRot2 = crypto_context.Decrypt(ciphertext_rot2,key_pair.secretKey)
-plaintextRot3 = crypto_context.Decrypt(ciphertext_rot3,key_pair.secretKey)
-plaintextRot4 = crypto_context.Decrypt(ciphertext_rot4,key_pair.secretKey)
+    # Decrypt the result of rotations
+    plaintextRot1 = crypto_context.Decrypt(ciphertext_rot1, key_pair.secretKey)
+    plaintextRot2 = crypto_context.Decrypt(ciphertext_rot2, key_pair.secretKey)
+    plaintextRot3 = crypto_context.Decrypt(ciphertext_rot3, key_pair.secretKey)
+    plaintextRot4 = crypto_context.Decrypt(ciphertext_rot4, key_pair.secretKey)
+
+    plaintextRot1.SetLength(len(vector_of_ints1))
+    plaintextRot2.SetLength(len(vector_of_ints1))
+    plaintextRot3.SetLength(len(vector_of_ints1))
+    plaintextRot4.SetLength(len(vector_of_ints1))
+
+    print("Plaintext #1: " + str(plaintext1))
+    print("Plaintext #2: " + str(plaintext2))
+    print("Plaintext #3: " + str(plaintext3))
+
+    # Output Results
+    print("\nResults of homomorphic computations")
+    print("#1 + #2 + #3 = " + str(plaintext_add_result))
+    print("#1 * #2 * #3 = " + str(plaintext_mult_result))
+    print("Left rotation of #1 by 1 = " + str(plaintextRot1))
+    print("Left rotation of #1 by 2 = " + str(plaintextRot2))
+    print("Right rotation of #1 by 1 = " + str(plaintextRot3))
+    print("Right rotation of #1 by 2 = " + str(plaintextRot4))
 
 
-plaintextRot1.SetLength(len(vector_of_ints1))
-plaintextRot2.SetLength(len(vector_of_ints1))
-plaintextRot3.SetLength(len(vector_of_ints1))
-plaintextRot4.SetLength(len(vector_of_ints1))
-
-print("Plaintext #1: " + str(plaintext1))
-print("Plaintext #2: " + str(plaintext2))
-print("Plaintext #3: " + str(plaintext3))
-
-# Output Results
-print("\nResults of homomorphic computations")
-print("#1 + #2 + #3 = " + str(plaintext_add_result))
-print("#1 * #2 * #3 = " + str(plaintext_mult_result))
-print("Left rotation of #1 by 1 = " + str(plaintextRot1))
-print("Left rotation of #1 by 2 = " + str(plaintextRot2))
-print("Right rotation of #1 by 1 = " + str(plaintextRot3))
-print("Right rotation of #1 by 2 = " + str(plaintextRot4))
+if __name__ == "__main__":
+    main()
--- examples/pke/simple-real-numbers-serial.py
+++ examples/pke/simple-real-numbers-serial.py
@@ -1,33 +1,37 @@
 from openfhe import *
-
+import os
+from pathlib import Path
+import tempfile
 
 # NOTE:
 # If running locally, you may want to replace the "hardcoded" datafolder with
 # the datafolder location below which gets the current working directory
 
 # Save-Load locations for keys
-datafolder = 'demoData'
-ccLocation = '/cryptocontext.txt'
-pubKeyLocation = '/key_pub.txt' # Pub key
-multKeyLocation = '/key_mult.txt' # relinearization key
-rotKeyLocation = '/key_rot.txt' # automorphism / rotation key
+datafolder = "demoData"
+ccLocation = "/cryptocontext.txt"
+pubKeyLocation = "/key_pub.txt"  # Pub key
+multKeyLocation = "/key_mult.txt"  # relinearization key
+rotKeyLocation = "/key_rot.txt"  # automorphism / rotation key
 
 # Save-load locations for RAW ciphertexts
-cipherOneLocation = '/ciphertext1.txt'
-cipherTwoLocation = '/ciphertext2.txt'
+cipherOneLocation = "/ciphertext1.txt"
+cipherTwoLocation = "/ciphertext2.txt"
 
 # Save-load locations for evaluated ciphertexts
-cipherMultLocation = '/ciphertextMult.txt'
-cipherAddLocation = '/ciphertextAdd.txt'
-cipherRotLocation = '/ciphertextRot.txt'
-cipherRotNegLocation = '/ciphertextRotNegLocation.txt'
-clientVectorLocation = '/clientVectorFromClient.txt'
+cipherMultLocation = "/ciphertextMult.txt"
+cipherAddLocation = "/ciphertextAdd.txt"
+cipherRotLocation = "/ciphertextRot.txt"
+cipherRotNegLocation = "/ciphertextRotNegLocation.txt"
+clientVectorLocation = "/clientVectorFromClient.txt"
+
 
 # Demarcate - Visual separator between the sections of code
 def demarcate(msg):
     print("**************************************************\n")
     print(msg)
     print("**************************************************\n")
+
 
 """
 serverSetupAndWrite(multDepth, scaleModSize, batchSize)
@@ -40,6 +44,8 @@ serverSetupAndWrite(multDepth, scaleModSize, batchSize)
     :param batchSize: batch size to use
     :return Tuple<cryptoContext, keyPair>
 """
+
+
 def serverSetupAndWrite(multDepth, scaleModSize, batchSize):
 
     parameters = CCParamsCKKSRNS()
@@ -75,7 +81,7 @@ def serverSetupAndWrite(multDepth, scaleModSize, batchSize):
     serverP2 = serverCC.MakeCKKSPackedPlaintext(vec2)
     serverP3 = serverCC.MakeCKKSPackedPlaintext(vec3)
 
-    print("Plaintext version of first vector: "+ str(serverP1))
+    print("Plaintext version of first vector: " + str(serverP1))
 
     print("Plaintexts have been generated from complex-double vectors")
 
@@ -93,7 +99,7 @@ def serverSetupAndWrite(multDepth, scaleModSize, batchSize):
     #      relinearization (eval mult keys)
     #      rotation keys
     #      Some of the ciphertext
-    #    
+    #
     #      We serialize all of them to files
     ###
     demarcate("Part 2: Data Serialization (server)")
@@ -116,21 +122,23 @@ def serverSetupAndWrite(multDepth, scaleModSize, batchSize):
 
     if not SerializeToFile(datafolder + cipherOneLocation, serverC1, BINARY):
         raise Exception("Error writing ciphertext 1")
-    
+
     if not SerializeToFile(datafolder + cipherTwoLocation, serverC2, BINARY):
         raise Exception("Error writing ciphertext 2")
-    
+
     return (serverCC, serverKP, len(vec1))
 
+
 ###
- # clientProcess
- #  - deserialize data from a file which simulates receiving data from a server
- # after making a request
- #  - we then process the data by doing operations (multiplication, addition,
- # rotation, etc)
- #  - !! We also create an object and encrypt it in this function before sending
- # it off to the server to be decrypted
+# clientProcess
+#  - deserialize data from a file which simulates receiving data from a server
+# after making a request
+#  - we then process the data by doing operations (multiplication, addition,
+# rotation, etc)
+#  - !! We also create an object and encrypt it in this function before sending
+# it off to the server to be decrypted
 ###
+
 
 def clientProcess():
     # clientCC = CryptoContext()
@@ -140,33 +148,45 @@ def clientProcess():
 
     clientCC, res = DeserializeCryptoContext(datafolder + ccLocation, BINARY)
     if not res:
-        raise Exception(f"I cannot deserialize the cryptocontext from {datafolder+ccLocation}")
+        raise Exception(
+            f"I cannot deserialize the cryptocontext from {datafolder+ccLocation}"
+        )
 
     print("Client CC deserialized")
 
-    #clientKP = KeyPair()
+    # clientKP = KeyPair()
     # We do NOT have a secret key. The client
     # should not have access to this
     clientPuclicKey, res = DeserializePublicKey(datafolder + pubKeyLocation, BINARY)
     if not res:
-        raise Exception(f"I cannot deserialize the public key from {datafolder+pubKeyLocation}")
+        raise Exception(
+            f"I cannot deserialize the public key from {datafolder+pubKeyLocation}"
+        )
     print("Client KP deserialized\n")
 
     if not clientCC.DeserializeEvalMultKey(datafolder + multKeyLocation, BINARY):
-        raise Exception(f"Cannot deserialize eval mult keys from {datafolder+multKeyLocation}")
+        raise Exception(
+            f"Cannot deserialize eval mult keys from {datafolder+multKeyLocation}"
+        )
     print("Deserialized eval mult keys\n")
 
     if not clientCC.DeserializeEvalAutomorphismKey(datafolder + rotKeyLocation, BINARY):
-        raise Exception(f"Cannot deserialize eval automorphism keys from {datafolder+rotKeyLocation}")
-    
+        raise Exception(
+            f"Cannot deserialize eval automorphism keys from {datafolder+rotKeyLocation}"
+        )
+
     clientC1, res = DeserializeCiphertext(datafolder + cipherOneLocation, BINARY)
     if not res:
-        raise Exception(f"Cannot deserialize the ciphertext from {datafolder+cipherOneLocation}")
+        raise Exception(
+            f"Cannot deserialize the ciphertext from {datafolder+cipherOneLocation}"
+        )
     print("Deserialized ciphertext 1\n")
 
     clientC2, res = DeserializeCiphertext(datafolder + cipherTwoLocation, BINARY)
     if not res:
-        raise Exception(f"Cannot deserialize the ciphertext from {datafolder+cipherTwoLocation}")
+        raise Exception(
+            f"Cannot deserialize the ciphertext from {datafolder+cipherTwoLocation}"
+        )
     print("Deserialized ciphertext 2\n")
 
     clientCiphertextMult = clientCC.EvalMult(clientC1, clientC2)
@@ -185,9 +205,12 @@ def clientProcess():
     SerializeToFile(datafolder + cipherAddLocation, clientCiphertextAdd, BINARY)
     SerializeToFile(datafolder + cipherRotLocation, clientCiphertextRot, BINARY)
     SerializeToFile(datafolder + cipherRotNegLocation, clientCiphertextRotNeg, BINARY)
-    SerializeToFile(datafolder + clientVectorLocation, clientInitializedEncryption, BINARY)
+    SerializeToFile(
+        datafolder + clientVectorLocation, clientInitializedEncryption, BINARY
+    )
 
     print("Serialized all ciphertexts from client\n")
+
 
 ###
 #  serverVerification
@@ -199,22 +222,42 @@ def clientProcess():
 # @return
 #  5-tuple of the plaintexts of various operations
 ##
-def serverVerification(cc,kp,vectorSize):
-    
-    serverCiphertextFromClient_Mult, res = DeserializeCiphertext(datafolder + cipherMultLocation, BINARY)
-    serverCiphertextFromClient_Add, res = DeserializeCiphertext(datafolder + cipherAddLocation, BINARY)
-    serverCiphertextFromClient_Rot, res = DeserializeCiphertext(datafolder + cipherRotLocation, BINARY)
-    serverCiphertextFromClient_RotNeg, res = DeserializeCiphertext(datafolder + cipherRotNegLocation, BINARY)
-    serverCiphertextFromClient_Vec, res = DeserializeCiphertext(datafolder + clientVectorLocation, BINARY)
+def serverVerification(cc, kp, vectorSize):
+
+    serverCiphertextFromClient_Mult, res = DeserializeCiphertext(
+        datafolder + cipherMultLocation, BINARY
+    )
+    serverCiphertextFromClient_Add, res = DeserializeCiphertext(
+        datafolder + cipherAddLocation, BINARY
+    )
+    serverCiphertextFromClient_Rot, res = DeserializeCiphertext(
+        datafolder + cipherRotLocation, BINARY
+    )
+    serverCiphertextFromClient_RotNeg, res = DeserializeCiphertext(
+        datafolder + cipherRotNegLocation, BINARY
+    )
+    serverCiphertextFromClient_Vec, res = DeserializeCiphertext(
+        datafolder + clientVectorLocation, BINARY
+    )
     print("Deserialized all data from client on server\n")
 
     print("Part 5: Correctness Verification")
 
-    serverPlaintextFromClient_Mult = cc.Decrypt(kp.secretKey, serverCiphertextFromClient_Mult)
-    serverPlaintextFromClient_Add = cc.Decrypt(kp.secretKey, serverCiphertextFromClient_Add)
-    serverPlaintextFromClient_Rot = cc.Decrypt(kp.secretKey, serverCiphertextFromClient_Rot)
-    serverPlaintextFromClient_RotNeg = cc.Decrypt(kp.secretKey, serverCiphertextFromClient_RotNeg)
-    serverPlaintextFromClient_Vec = cc.Decrypt(kp.secretKey, serverCiphertextFromClient_Vec)
+    serverPlaintextFromClient_Mult = cc.Decrypt(
+        kp.secretKey, serverCiphertextFromClient_Mult
+    )
+    serverPlaintextFromClient_Add = cc.Decrypt(
+        kp.secretKey, serverCiphertextFromClient_Add
+    )
+    serverPlaintextFromClient_Rot = cc.Decrypt(
+        kp.secretKey, serverCiphertextFromClient_Rot
+    )
+    serverPlaintextFromClient_RotNeg = cc.Decrypt(
+        kp.secretKey, serverCiphertextFromClient_RotNeg
+    )
+    serverPlaintextFromClient_Vec = cc.Decrypt(
+        kp.secretKey, serverCiphertextFromClient_Vec
+    )
 
     serverPlaintextFromClient_Mult.SetLength(vectorSize)
     serverPlaintextFromClient_Add.SetLength(vectorSize)
@@ -222,14 +265,27 @@ def serverVerification(cc,kp,vectorSize):
     serverPlaintextFromClient_Rot.SetLength(vectorSize + 1)
     serverPlaintextFromClient_RotNeg.SetLength(vectorSize + 1)
 
-    return (serverPlaintextFromClient_Mult, 
-            serverPlaintextFromClient_Add, 
-            serverPlaintextFromClient_Vec, 
-            serverPlaintextFromClient_Rot, 
-            serverPlaintextFromClient_RotNeg)
+    return (
+        serverPlaintextFromClient_Mult,
+        serverPlaintextFromClient_Add,
+        serverPlaintextFromClient_Vec,
+        serverPlaintextFromClient_Rot,
+        serverPlaintextFromClient_RotNeg,
+    )
+
 
 def main():
-    print(f"This program requires the subdirectory `{datafolder}' to exist, otherwise you will get\n an error writing serializations.")
+    global datafolder
+    with tempfile.TemporaryDirectory() as td:
+        datafolder = td + "/" + datafolder
+        os.makedirs(datafolder)
+        main_action()
+
+
+def main_action():
+    print(
+        f"This program requires the subdirectory `{datafolder}' to exist, otherwise you will get\n an error writing serializations."
+    )
 
     # Set main params
     multDepth = 5
@@ -246,7 +302,9 @@ def main():
     cipherRotResIdx = 3
     cipherRotNegResIdx = 4
 
-    demarcate("Part 1: Cryptocontext generation, key generation, data encryption \n(server)")
+    demarcate(
+        "Part 1: Cryptocontext generation, key generation, data encryption \n(server)"
+    )
 
     tupleCryptoContext_KeyPair = serverSetupAndWrite(multDepth, scaleModSize, batchSize)
     cc = tupleCryptoContext_KeyPair[cryptoContextIdx]
@@ -269,19 +327,13 @@ def main():
     # vec2: [12.5, 13.5, 14.5, 15.5]
 
     print(multRes)  # EXPECT: 12.5, 27.0, 43.5, 62
-    print(addRes)   # EXPECT: 13.5, 15.5, 17.5, 19.5
-    print(vecRes)   # EXPECT:  [1,2,3,4]
+    print(addRes)  # EXPECT: 13.5, 15.5, 17.5, 19.5
+    print(vecRes)  # EXPECT:  [1,2,3,4]
 
     print("Displaying 5 elements of a 4-element vector to illustrate rotation")
-    print(rotRes)     # EXPECT: [2, 3, 4, noise, noise]
+    print(rotRes)  # EXPECT: [2, 3, 4, noise, noise]
     print(rotNegRes)  # EXPECT: [noise, 1, 2, 3, 4]
+
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
