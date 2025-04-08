9896f101623f0e113730b56221fa72916ea33096
Yuriy Polyakov <ypolyakod@dualitytech.com>
2025-04-07 20:17:14 -0400

    updated the example

--- examples/pke/test-threshold-2p.py
+++ examples/pke/test-threshold-2p.py
@@ -43,9 +43,9 @@ def main():
 
     # Party 2
     pk_received = DeserializePublicKeyString(pk_to_share, sertype) # Received pk_to_share from Party 1
-   # keys_p2 = cc_p2.MultipartyKeyGen(pk_received, False, True)
+    keys_p2 = cc_p2.MultipartyKeyGen(pk_received, False, True)
 
-  #  mp_public_key = cc_p1.MultiAddPubKeys(keys_p1.publicKey, keys_p2.publicKey, keys_p2.publicKey.GetKeyTag())
+    mp_public_key = cc_p1.MultiAddPubKeys(keys_p1.publicKey, keys_p2.publicKey, keys_p2.publicKey.GetKeyTag())
 
 
 
@@ -54,9 +54,10 @@ def main():
     cc_p1.EvalSumKeyGen(keys_p1.secretKey)
     evalSumKeys_p1 = cc_p1.GetEvalSumKeyMap(keys_p1.secretKey.GetKeyTag())
     evalSumKeys_to_share = Serialize(evalSumKeys_p1, sertype)     # Send evalSumKeys_to_share to Party 2
+    testkeys = DeserializeEvalKeyMapString(evalSumKeys_to_share, sertype)
     
     # # Party 2
-    evalSumKeysB = cc_p2.MultiEvalSumKeyGen(keys_p2.secretKey, DeserializeEvalKeyString(evalSumKeys_to_share, sertype), keys_p2.publicKey.GetKeyTag())
+    evalSumKeysB = cc_p2.MultiEvalSumKeyGen(keys_p2.secretKey, DeserializeEvalKeyMapString(evalSumKeys_to_share, sertype), keys_p2.publicKey.GetKeyTag())
 
 
 if __name__ == "__main__":
